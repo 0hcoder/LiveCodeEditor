99a5a507204f0916edfd887d0840ae2e07dcabe3
Ankit Rawat <dev.ankurawat@gmail.com>
2026-04-04 00:04:21 +0530

    move gitignore to root

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,19 @@
 import "./App.css";
+import { Editor } from "@monaco-editor/react";
 const App = () => {
-  return <main className="h-screen w-full bg-gray-950">
-    
-  </main>;
+  return (
+    <main className="flex h-screen w-full bg-gray-950 p-4 gap-2">
+      <aside className="h-full w-1/4 bg-amber-50 rounded-lg"></aside>
+      <section className=" bg-neutral-900 h-full w-3/4 rounded-lg">
+        <Editor
+          height="100%"
+          defaultLanguage="javascript"
+          theme="vs-dark"
+          defaultValue="// Code Here...."
+        />
+      </section>
+    </main>
+  );
 };
 
 export default App;
